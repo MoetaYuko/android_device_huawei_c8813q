4fd02cb1a6a7cdc195794ed2869cf7fecf2d054c
dianlujitao <dianlujitao@gmail.com>
2014-02-17 21:51:52 +0800

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "huawei C8813Q"
+#define BTM_DEF_LOCAL_NAME   "Huawei C8813Q"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
